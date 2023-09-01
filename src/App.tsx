abb8684e9039b852f832002a0a33708e1d350561
eliasdev <eliasgomez.developer@gmail.com>
2023-09-01 04:01:12 -0600

    deploy updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css';
 import Menu from './pages/menu'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const About = () => <div>About Page</div>;
 const Contact = () => <div>Contact Page</div>;
@@ -9,16 +10,21 @@ const Contact = () => <div>Contact Page</div>;
 
 function App() {
   return (
-    <div className="App">
-      <div className='mosaicBackground'></div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <Helmet>
+          <title>Puro Sabor | Ordená en Línea 🛍️📲</title>
+      </Helmet>
+      <div className="App">
+        <div className='mosaicBackground'></div>
+        <Router basename="/index.html">
+          <Routes>
+            <Route path="/" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
