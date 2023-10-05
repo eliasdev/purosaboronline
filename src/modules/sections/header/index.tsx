9b30b07cca81c9f1954d4d09d9778433fc8241b2
eliasdev <eliasgomez.developer@gmail.com>
2023-10-05 03:04:16 -0600

    updates

--- src/modules/sections/header/index.tsx
+++ src/modules/sections/header/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, useMediaQuery, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, useMediaQuery, useTheme, Modal, Backdrop } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { WindowOutlined } from '@mui/icons-material';
 // ... (previous imports)
 
 function Header() {
     const theme = useTheme();
+    const [isScheduleOpen, setIsScheduleOpen] = useState(false);
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null); // Define the type for anchorEl
     const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -22,9 +23,34 @@ function Header() {
       window.open('https://wa.me/50685194028', '_blank');
       closeMenu();
     };
+
+    const openSchedule = () => {
+      setIsScheduleOpen(!isScheduleOpen);
+    }
   
     return (
       <AppBar position="static" sx={{ backgroundColor: '#FF0000' }}>
+        <Modal open={isScheduleOpen} onClose={() => setIsScheduleOpen(!isScheduleOpen)}>
+          <div>
+            <Backdrop open={isScheduleOpen} onClick={() => setIsScheduleOpen(!isScheduleOpen)} style={{ zIndex: 9999 }} />
+              <div className="modal-overlay">
+                <div
+                  className="modal-content"
+                  style={{ width: isMobile ? '90%' : '60%' }}
+                >
+                    <div>
+                      <Typography
+                        sx={{ fontSize: { xs: '1em', lg: '2em' }, textAlign: 'center' }}
+                        variant="h5"
+                        gutterBottom
+                      >
+                        Nuestro horario de trabajo es de<br/>Jueves, Viernes y Sábado 5PM a 9PM ✅
+                      </Typography>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </Modal>
         <Toolbar>
           <Typography align='left' variant="h6" component="div" sx={{ flexGrow: 1 }}>
           🍔 Puro Sabor | Comida Artesanal
@@ -47,12 +73,14 @@ function Header() {
               >
                 <MenuItem onClick={downloadMenu}>Descargar Menú 🍔</MenuItem>
                 <MenuItem onClick={contactUs}>Contacto 📲</MenuItem>
+                <MenuItem onClick={openSchedule}>Horario ⏰</MenuItem>
               </Menu>
             </>
           ) : (
             <>
               <MenuItem onClick={downloadMenu}>Descargar Menú 🍔</MenuItem>
               <MenuItem onClick={contactUs}>Contacto 📲</MenuItem>
+              <MenuItem onClick={openSchedule}>Horario ⏰</MenuItem>
             </>
           )}
         </Toolbar>
