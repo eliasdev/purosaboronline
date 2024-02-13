f4bd9c05045fb6ff29533977be3f45a6eb29cfac
eliasdev <eliasgomez.developer@gmail.com>
2024-02-13 10:28:41 -0600

    Sh*t down

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Typography } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <Typography>
+    Estimados clientes y amigos de Puro Sabor,
+
+Queremos informarles que hemos decidido cerrar temporalmente nuestras puertas para buscar un nuevo espacio que nos permita brindar un mejor servicio. Estamos comprometidos con mejorar y expandir nuestras instalaciones para ofrecer una experiencia más acogedora. Continuaremos informándoles sobre cualquier desarrollo a través de nuestras redes sociales y sitio web. Agradecemos su comprensión y apoyo. Esperamos regresar pronto con una experiencia renovada y deliciosa.
+
+Atentamente,
+Josue Paniagua/Elías Gomez
+Socios comerciales de Puro Sabor
+    </Typography>
   </React.StrictMode>
 );
 
